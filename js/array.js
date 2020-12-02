82e3e1f23ec9b278f0ef9b7600e4148adfccebf4
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2020-12-02 19:29:03 +0200

    Array push()

---
+++ js/array.js
@@ -0,0 +1,22 @@
+function printArray(a) {
+  document.write("[ ");
+  for (let i = 0; i < a.length; i++) {
+    document.write(`${a[i]}`);
+
+    if (i != a.length - 1) {
+      document.write(", ");
+    }
+  }
+  document.write(" ]<br />");
+}
+
+var arr = [1, 5, 7, 2, 0, -1, 8];
+printArray(arr);
+
+arr.push(13);
+printArray(arr);
+
+arr.push(17, 12, 42);
+printArray(arr);
+
+
