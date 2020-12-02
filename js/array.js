58a6c3f27a855a1865227561e9a3461f3884e7cd
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2020-12-02 20:25:51 +0200

    Array sort() and map()

--- js/array.js
+++ js/array.js
@@ -1,13 +1,30 @@
 function printArray(a) {
   document.write("[ ");
   for (let i = 0; i < a.length; i++) {
-    document.write(`${a[i]}`);
+    document.write(`${objToString(a[i])}`);
 
     if (i != a.length - 1) {
-      document.write(", ");
+      if (typeof a[i] == "object") {
+        document.write("<br />");
+      } else {
+        document.write(", ");
+      }
     }
   }
   document.write(" ]<br />");
+}
+
+function objToString(obj) {
+  if (typeof obj == "object") {
+    let objStr = "";
+    for (let prop in obj) {
+      objStr+= `, ${prop}: ${obj[prop]}`;
+    }
+
+    return objStr.substr(2);
+  }
+
+  return obj;
 }
 
 var arr = [1, 5, 7, 2, 0, -1, 8];
@@ -30,3 +47,70 @@ printArray(arr);
 var el1 = arr.shift();
 printArray(arr);
 document.write(el1 + "<br />");
+
+arr.sort();
+printArray(arr);
+
+arr.sort((a, b) => {
+  return a - b;
+});
+printArray(arr);
+
+var people = [
+  {
+    name: "Anna",
+    gender: "female",
+    age: 21
+  },
+  {
+    name: "Boris",
+    gender: "male",
+    age: 45
+  },
+  {
+    name: "Misha",
+    gender: "male",
+    age: 45
+  },
+  {
+    name: "Olga",
+    gender: "female",
+    age: 45
+  },
+  {
+    name: "Elena",
+    gender: "female",
+    age: 19
+  },
+];
+printArray(people);
+
+people.sort((el1, el2) => {
+  var ageComp = el1.age - el2.age;
+  if (ageComp != 0) {
+    return ageComp;
+  }
+
+  return -el1.name.localeCompare(el2.name);
+});
+printArray(people);
+
+var peopleWithSkinColor = people.map((el) => {
+  if (el.gender == "female") {
+    el.skin = "black";
+  } else {
+    el.skin = "orange";
+  }
+
+  return el;
+});
+printArray(peopleWithSkinColor);
+
+let numbers = [5, 3, 1, 4, 2];
+let squares = numbers.map((a) => {
+  return a**2; // a*a
+}).sort((a, b) => {
+  return a - b;
+});
+printArray(numbers);
+printArray(squares);
